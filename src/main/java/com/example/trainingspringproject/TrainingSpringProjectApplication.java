a1d5907a75a879e7273e7fd2e33137abcb5e1480
Andrey Kusakin <elementalius_2k@mail.ru>
2022-09-08 12:49:45 +0300

    Initial commit

---
+++ src/main/java/com/example/trainingspringproject/TrainingSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.trainingspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingSpringProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trainingspringproject/TrainingSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainingspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingSpringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
