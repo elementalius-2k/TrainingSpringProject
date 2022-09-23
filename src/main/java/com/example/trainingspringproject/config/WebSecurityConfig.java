7853d64adcd58e860805ba4a099438dab0c86912
Andrey Kusakin <elementalius_2k@mail.ru>
2022-09-23 20:46:26 +0300

    Add web security configuration

---
+++ src/main/java/com/example/trainingspringproject/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.trainingspringproject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .authorizeRequests().anyRequest().permitAll();
+    }
+}
